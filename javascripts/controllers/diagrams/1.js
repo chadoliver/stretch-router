69fcf25e82edd803d4f873dcee56ef266f545877
chad oliver <accounts@chadoliver.co.nz>
2013-08-10 07:09:51 -0400

    implemented a simple static wrapping algorithm. There are plenty of bugs, but it works.

--- javascripts/controllers/diagrams/1.js
+++ javascripts/controllers/diagrams/1.js
@@ -3,11 +3,39 @@ var frontpage = angular.module('diagram1', []);
 frontpage.controller('diagram1Controller', function ($scope) {
 
     var Obstacle = function(x, y, radius) {
+        
+        var TRACK_SPACING = 20;
+        this.radius = radius;
+        this.wrapRadius = radius + TRACK_SPACING;
+        this.x = x;
+        this.y = y;
+        
         this.pad = two.makeCircle(x, y, radius);
-        this.keepout = two.makeCircle(x, y, radius+20);
+        this.keepout = two.makeCircle(x, y, radius+TRACK_SPACING);
         this.keepout.opacity = 0.5;
         this.keepout.noFill();
     };
+    
+    var wrapCWtoACW = function(obstacleA, obstacleB) {
+        
+        var vector = {
+            x: obstacleA.x-obstacleB.x,
+            y: obstacleA.y-obstacleB.y,
+        };
+        var distance = Math.sqrt(vector.x*vector.x + vector.y*vector.y);
+        var alpha = Math.asin((obstacleA.wrapRadius + obstacleB.wrapRadius) / distance); 
+        
+        return {
+            A: {
+                x: obstacleA.x + obstacleA.wrapRadius*Math.sin(alpha),
+                y: obstacleA.y + obstacleA.wrapRadius*Math.cos(alpha),
+            },
+            B: {
+                x: obstacleB.x - obstacleB.wrapRadius*Math.sin(alpha),
+                y: obstacleB.y - obstacleB.wrapRadius*Math.cos(alpha),
+            },
+        };
+    }
     
     console.log('fooo');
     
@@ -25,7 +53,13 @@ frontpage.controller('diagram1Controller', function ($scope) {
     });
     two.appendTo(container);
     
-    var circle1 = new Obstacle(viewport.x/2 - 200, viewport.y/2, 30);
-    var circle2 = new Obstacle(viewport.x/2 + 200, viewport.y/2, 30);
-    var circle3 = new Obstacle(viewport.x/2, viewport.y/2 + 30, 80);
+    var obstacle1 = new Obstacle(viewport.x/2 - 200,  viewport.y/2 - 20,  30);
+    var obstacle2 = new Obstacle(viewport.x/2,        viewport.y/2 ,      80);
+    var obstacle3 = new Obstacle(viewport.x/2 + 200,  viewport.y/2 + 20,  50);
+    
+    var pointset1 = wrapCWtoACW(obstacle2, obstacle3);
+    two.makeLine(pointset1.A.x, pointset1.A.y, pointset1.B.x, pointset1.B.y);
+    
+    var pointset2 = wrapCWtoACW(obstacle2, obstacle1);
+    two.makeLine(pointset2.A.x, pointset2.A.y, pointset2.B.x, pointset2.B.y);
 });

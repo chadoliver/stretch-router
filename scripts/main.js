7aa212b3200404e8e4b8fac613ea336797215d3e
chad oliver <accounts@chadoliver.co.nz>
2013-08-24 06:26:38 -0400

    imported changes from the master branch.

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,8 @@ var constants;
     constants.CLOCKWISE = 'CLOCKWISE';
     constants.CENTER = 'CENTER';
 
-    constants.TRACK_SPACING = 20;
-    constants.TRACK_WIDTH = 6;
+    constants.TRACK_SPACING = 27.5;
+    constants.TRACK_WIDTH = 10;
 })(constants || (constants = {}));
 ;
 /// <reference path="./common.ts"/>
@@ -165,10 +165,11 @@ var obstacle;
                 return label;
             };
 
-            var ringOneRadius = this.radius + c.TRACK_SPACING;
+            var ringOneRadius = this.radius + c.TRACK_SPACING - c.TRACK_WIDTH / 2;
 
             var element;
 
+            var dotRadius = 7.5;
             element = buildCircle(this.x, this.y, this.radius);
             scene.groups.obstacles.appendChild(element);
 
@@ -468,18 +469,18 @@ var examples;
             });
 
             var obs = PaintableSet({
-                r1c1: new Obstacle(194, 75, 20),
-                r1c2: new Obstacle(294, 75, 20),
-                r1c3: new Obstacle(394, 75, 20),
-                r1c4: new Obstacle(494, 75, 20),
-                r2c1: new Obstacle(194, 175, 20),
-                r2c2: new Obstacle(294, 175, 20),
-                r2c3: new Obstacle(394, 175, 20),
-                r2c4: new Obstacle(494, 175, 20),
-                r3c1: new Obstacle(194, 275, 20),
-                r3c2: new Obstacle(294, 275, 20),
-                r3c3: new Obstacle(394, 275, 20),
-                r3c4: new Obstacle(494, 275, 20)
+                r1c1: new Obstacle(194, 75, 7.5),
+                r1c2: new Obstacle(294, 75, 7.5),
+                r1c3: new Obstacle(394, 75, 7.5),
+                r1c4: new Obstacle(494, 75, 7.5),
+                r2c1: new Obstacle(194, 175, 7.5),
+                r2c2: new Obstacle(294, 175, 7.5),
+                r2c3: new Obstacle(394, 175, 7.5),
+                r2c4: new Obstacle(494, 175, 7.5),
+                r3c1: new Obstacle(194, 275, 7.5),
+                r3c2: new Obstacle(294, 275, 7.5),
+                r3c3: new Obstacle(394, 275, 7.5),
+                r3c4: new Obstacle(494, 275, 7.5)
             });
 
             var tracks = PaintableSet({
@@ -489,7 +490,7 @@ var examples;
             tracks.foo.clockwise(obs.r1c2).clockwise(obs.r2c2).anticlockwise(obs.r2c1).center(obs.r3c2).center(obs.r3c3).anticlockwise(obs.r3c4).anticlockwise(obs.r2c4).clockwise(obs.r2c3).center(obs.r1c4);
 
             obs.paint(scene);
-            tracks.foo.paint(scene);
+            tracks.paint(scene);
         },
         secondExample: function (element) {
             var scene = new Scene({
@@ -508,13 +509,46 @@ var examples;
             });
 
             var tracks = PaintableSet({
-                foo: new Track(obs.topStart, 10),
-                bar: new Track(obs.bottomStart, 10)
+                foo: new Track(obs.topStart, c.TRACK_WIDTH),
+                bar: new Track(obs.bottomStart, c.TRACK_WIDTH)
             });
 
             tracks.foo.anticlockwise(obs.topObstacle).clockwise(obs.bottomObstacle).center(obs.bottomEnd);
 
             tracks.bar.clockwise(obs.topStart).clockwise(obs.topObstacle).center(obs.topEnd);
+
+            obs.paint(scene);
+            tracks.paint(scene);
+        },
+        thirdExample: function (element) {
+            var scene = new Scene({
+                parent: element,
+                width: SVG_WIDTH,
+                height: 350
+            });
+
+            var obs = PaintableSet({
+                r1c1: new Obstacle(194, 75, 7.5),
+                r1c2: new Obstacle(294, 75, 7.5),
+                r1c3: new Obstacle(394, 75, 7.5),
+                r1c4: new Obstacle(494, 75, 7.5),
+                r2c1: new Obstacle(194, 175, 7.5),
+                r2c2: new Obstacle(294, 175, 7.5),
+                r2c3: new Obstacle(394, 175, 7.5),
+                r2c4: new Obstacle(494, 175, 7.5),
+                r3c1: new Obstacle(194, 275, 7.5),
+                r3c2: new Obstacle(294, 275, 7.5),
+                r3c3: new Obstacle(394, 275, 7.5),
+                r3c4: new Obstacle(494, 275, 7.5)
+            });
+
+            var tracks = PaintableSet({
+                foo: new Track(obs.r2c1, c.TRACK_WIDTH),
+                bar: new Track(obs.r1c2, c.TRACK_WIDTH),
+                baz: new Track(obs.r1c3, c.TRACK_WIDTH)
+            });
+
+            tracks.foo.clockwise(obs.r2c2).clockwise(obs.r3c3).anticlockwise(obs.r2c2).center(obs.r3c1);
 
             obs.paint(scene);
             tracks.paint(scene);

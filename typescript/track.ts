8b5464f822fe5627ec598403d9a574a8bbeb3806
chad oliver <accounts@chadoliver.co.nz>
2013-08-24 09:14:58 -0400

    pulled low-level path stuff out into a separate class.

--- typescript/track.ts
+++ typescript/track.ts
@@ -2,6 +2,7 @@
 /// <reference path="./stretch.ts"/>
 /// <reference path="./scene.ts"/>
 /// <reference path="./obstacle.ts"/>
+/// <reference path="./path.ts"/>
 
 module track {
 
@@ -10,6 +11,7 @@ module track {
     import Stretch = stretch.Stretch;
     import Scene = scene.Scene;
     import Obstacle = obstacle.Obstacle;
+    import Path = path.Path;
 
     export class Track {
         
@@ -17,59 +19,23 @@ module track {
         
         private currentObs;
         private currentWrapMode;
-        private currentLineEndPoint: g.Point;
-        private pathElement: Element;
-        private instructions: any[] = [];        // all elements will eventually be converted to strings, but they can be stored in their native type.
+        private currentLineEndPoint :g.Point;
+        private path :Path;
         
         constructor(startObs:g.Point, width) {
             
             this.width = width;
-            
             this.currentObs = startObs;
             this.currentWrapMode = c.CENTER;
-            
-            this.pathElement = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-            this.moveTo(startObs);
-        }
-        
-        private moveTo(dest: g.Point) {
-            this.instructions.push("M", dest.x, dest.y);
-            this.currentLineEndPoint = dest;
-        }
-        
-        private lineTo(dest: g.Point) {
-            
-            this.instructions.push("L ", dest.x, dest.y);
-            this.currentLineEndPoint = dest;
-        }
-        
-        private arcTo(dest: g.Point) {
-        
-            // A rx ry x-axis-rotation large-arc-flag sweep-flag x y
-            
-            if (this.currentWrapMode === c.CENTER) return;     // no arc is required
-            
-            var effectiveRadius = this.currentObs.radius + c.TRACK_SPACING;
-            var angularDistance = this.currentObs.angularLengthOfArc(this.currentLineEndPoint, dest, this.currentWrapMode);
-            
-            var rx = effectiveRadius;
-            var ry = effectiveRadius;                           // This is a circle, so both axis are identical ...
-            var xAxisRot = 0;                                   // ... and rotation is meaningless.
-            var largeArcFlag = (angularDistance > Math.PI) ? 1 : 0;
-            var sweepFlag = (this.currentWrapMode === c.CLOCKWISE) ? 1 : 0;
-            var x = dest.x;
-            var y = dest.y;
-            
-            this.instructions.push("A", rx, ry, xAxisRot, largeArcFlag, sweepFlag, x, y);
-            this.currentLineEndPoint = dest;
+            this.path = new Path(startObs, width);
         }
         
         private nextSegment(nextObs:Obstacle, direction:string) {
             
             var stretch = new Stretch(this.currentObs, this.currentWrapMode, nextObs, direction);
             
-            this.arcTo(stretch.start);
-            this.lineTo(stretch.end);
+            this.path.arcTo(this.currentObs, stretch.start, this.currentWrapMode);
+            this.path.lineTo(stretch.end);
             
             this.currentObs = nextObs;
             this.currentWrapMode = direction;
@@ -98,11 +64,7 @@ module track {
         
         public paint(scene:Scene) {
             
-            var instructionString: string = this.instructions.join(" ");
-            
-            this.pathElement.setAttribute("d", instructionString);
-            this.pathElement.setAttribute("stroke-width", this.width.toString());
-            scene.groups.tracks.appendChild(this.pathElement);
+            this.path.paint(scene, 'tracks');
             
             return this;
         }

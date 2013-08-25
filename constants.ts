e3be91c90b25ce5a330249e5b80549d3a6813e18
chad oliver <accounts@chadoliver.co.nz>
2013-08-25 18:29:43 -0400

    moved interfaces into series/*, and can therefore give common.ts a more descriptive name.

---
+++ constants.ts
@@ -0,0 +1,10 @@
+module constants {
+    
+    export var ANTI_CLOCKWISE = 'ANTI_CLOCKWISE';
+    export var CLOCKWISE = 'CLOCKWISE';
+    export var CENTER = 'CENTER';
+
+    export var TRACK_SPACING = 27.5;
+    export var TRACK_WIDTH = 10;
+    
+}
